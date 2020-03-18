491e3037b311cdda3c53930ebf535b496cd150ad
abGitId <abhi.kadam397@gmail.com>
2020-03-18 07:06:18 +0000

    practice | add method added

--- Test.java
+++ Test.java
@@ -14,7 +14,13 @@ public static void main(String[] args) {
         num2 = sc.nextInt();
 
         sc.close();
-        sum = num1 + num2;
+        sum = add(num1, num2);
         System.out.println("Sum of these numbers: "+sum);
     }
+
+private static int add(int num1, int num2) {
+    int sum;
+    sum = num1 + num2;
+    return sum;
+}
 }
